0e4c0586bffe8b9e88e850f2ab676b348bdf7bdc
emhege <emhege@floridauniversitaria.es>
2024-02-06 11:51:18 +0100

    pusheo al cd bien

--- tests/enanaTest.php
+++ tests/enanaTest.php
@@ -33,6 +33,7 @@ class EnanaTest extends TestCase {
         #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
         #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
 
+        
     }
 
     public function testPocimaNoRevive() {
